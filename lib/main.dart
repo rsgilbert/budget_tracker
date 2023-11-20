055fdc54790951c56a0e6500f997bf9322707283
Gilbert Ssenyonjo <gssenyonjo@gmail.com>
2023-11-20 23:59:19 +0300

    Implemented _configureAmplify

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'package:amplify_api/amplify_api.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:budget_tracker/amplifyconfiguration.dart';
 import 'package:budget_tracker/models/BudgetEntry.dart';
+import 'package:budget_tracker/models/ModelProvider.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -9,7 +14,24 @@ Future<void> main() async {
 }
 
 Future<void> _configureAmplify() async {
-  // to be filled in
+  try {
+    // Create the API plugin
+    // If ModelProvider.instance is not available, try running 
+    // `amplify codegen models` from the root of project
+    final api = AmplifyAPI(modelProvider: ModelProvider.instance);
+
+    // Create the Auth plugin 
+    final auth = AmplifyAuthCognito();
+
+    // Add the plugins and configure Amplify for app 
+    await Amplify.addPlugins([api, auth]);
+    await Amplify.configure(amplifyconfig);
+
+    safePrint('Successfully configured amplify');
+  }
+  on Exception catch(e) {
+    safePrint('Error configuring amplify: $e');
+  }
 }
 
 class MyApp extends StatelessWidget {
